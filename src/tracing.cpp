43539e40a37dfa37c491ac9f7387506c751a9529
Kurt Böhm <kurbo96@gmail.com>
2025-08-29 10:23:26 +0200

    Revert the full library to an earlier approach which, while less safe in theory, actually worked in practice

--- src/tracing.cpp
+++ src/tracing.cpp
@@ -7,12 +7,11 @@
 #include <algorithm>
 #include <cstddef>
 #include <cstdlib>
+#include <deque>
 #include <mutex>
 #include <optional>
-#include <shared_mutex>
 #include <thread>
 #include <utility>
-#include <vector>
 
 #include "contador/contador.hpp"
 
@@ -21,12 +20,12 @@
 namespace {
 using Counter = std::pair<std::thread::id, std::size_t>;
 
-std::shared_mutex& counters_mutex() {
-  static std::shared_mutex mutex{};
+std::mutex& counters_mutex() {
+  static std::mutex mutex{};
   return mutex;
 }
-std::optional<std::vector<Counter>>& get_counters() {
-  static std::optional<std::vector<Counter>> data{};
+std::optional<std::deque<Counter>>& get_counters() {
+  static std::optional<std::deque<Counter>> data{};
   return data;
 }
 } // namespace
@@ -35,20 +34,16 @@ extern "C" {
 void free(void* p) {
   const auto tid = std::this_thread::get_id();
 
-  std::shared_lock shlck{counters_mutex()};
   auto& counters_opt = get_counters();
+
   if (counters_opt.has_value()) {
     auto& counters = *counters_opt;
     auto it = std::ranges::find_if(counters, [tid](Counter p) { return p.first == tid; });
 
     std::size_t* iptr{};
     if (it == counters.end()) {
-      shlck.unlock();
-      {
-        std::unique_lock lock{counters_mutex()};
-        iptr = &counters.emplace_back(tid, 0).second;
-      }
-      shlck.lock();
+      std::lock_guard lock{counters_mutex()};
+      iptr = &counters.emplace_back(tid, 0).second;
     } else {
       iptr = &it->second;
     }
@@ -63,7 +58,6 @@ void free(void* p) {
     }
     --i;
   }
-  shlck.unlock();
 
   auto free_ptr = get_free();
   free_ptr(p);
